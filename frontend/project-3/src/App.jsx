ce8eefc80b473d29409045ac0846c5fca5eeb96b
johnharding04 <john_harding04@tamu.edu>
2025-11-08 12:03:19 -0600

    Add basic Cashier View and Menu Board Pages

--- frontend/project-3/src/App.jsx
+++ frontend/project-3/src/App.jsx
@@ -1,18 +1,20 @@
 import { useState } from 'react'
 import './App.css'
+import CashierView from "./views/CashierView";
+import MenuBoard from "./views/MenuBoard.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [view, setView] = useState("menu");
 
   return (
-    <>
-      <div>
-         <h1>
-           Hello World!!!
-         </h1>
+    <div style={{ paddingBottom: "60px" }}>
+      {view === "menu" && <MenuBoard />}
+      {view === "cashier" && <CashierView />}
+      <div className="bottom-nav">
+        <button onClick={() => setView("menu")}>Menu Board</button>
+        <button onClick={() => setView("cashier")}>Cashier View</button>
       </div>
-      
-    </>
+    </div>
   )
 }
 
