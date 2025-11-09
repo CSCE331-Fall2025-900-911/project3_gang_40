d9fc1ffd81e9e1b178526ef983c84cf7ef702cf8
johnharding04 <john_harding04@tamu.edu>
2025-11-09 15:54:15 -0600

    Add price

--- frontend/project-3/src/Views/MenuBoard/MenuItem.jsx
+++ frontend/project-3/src/Views/MenuBoard/MenuItem.jsx
@@ -4,7 +4,7 @@ function MenuItem (props) {
     return(
         <div className="menuItem">
             <img src= {props.menu.imgUrl}/>
-            <h2>{props.menu.name}</h2>
+            <h2>{props.menu.name} - {props.menu.price}</h2>
             <p>{props.menu.toppings}</p>
         </div>
     )
