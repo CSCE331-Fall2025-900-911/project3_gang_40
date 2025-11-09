dd175f3b3a9f191c2abb9c4276af6a5b8762b579
JacobHardwick <jacobhardwick4@tamu.edu>
2025-11-08 20:33:38 -0600

    added connection to database

--- frontend/project-3/src/Views/Cashier/Cashier.jsx
+++ frontend/project-3/src/Views/Cashier/Cashier.jsx
@@ -1,25 +1,45 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './Cashier.css'
 
 function Cashier({ onBack }) {
-  const drinks = ['Classic Pearl Milk Tea', 'Honey Pearl Milk Tea', 'Coffee Creama', 'Coffee Milk Tea w/ Coffee Jelly', 
-    'Hokkaido Pearl Milk Tea', 'Thai Pearl Milk Tea', 'Taro Pearl Milk Tea', 'Mango Green Milk Tea', 'Golden Retriver',
-    'Coconut Pearl Milk Tea', 'Classic Tea', 'Honey Tea', 'Mango Green Tea', 'Passion Chess', 'Berry Lychee Burst',
-    'Peach Tea w/ Honey Jelly', 'Mango & Passion Fruit Tea', 'Honey Lemonade', 'Tiger Boba', 'Stawberry Coconut'
-  ]
+  // create empty list for drinks and toppings
+  const [drinks, setDrinks] = useState([])
+  const [toppings, setToppings] = useState([])
+
+  // populate drinks from database
+  useEffect(() => {
+    fetch('http://localhost:5000/api/drinks')
+      .then(res => res.json())
+      .then(data => {
+        console.log('Fetched drinks:', data);
+        setDrinks(data);
+      })
+      .catch(err => console.error('Error fetching drinks:', err))
+  }, []);
+
+   // populate toppings from database
+  useEffect(() => {
+    fetch('http://localhost:5000/api/toppings')
+      .then(res => res.json())
+      .then(data => {
+        console.log('Fetched toppings:', data);
+        const names = data.map(t => t.topping_name);
+        setToppings(names);
+      })
+      .catch(err => console.error('Error fetching toppings:', err))
+  }, []);
+
   const [cart, setCart] = useState([])
   const [selectedDrink, setSelectedDrink] = useState(null);
+  // set default drink modification values
   const [modifications, setModifications] = useState({
-    size: 'Medium', sweetness: 'Normal (100%)', ice: 'Regular', toppings: [], quantity: '1'
+    size: 'Medium', sweetness: 'Normal (100%)', ice: 'Regular', topping: 'No Toppings', quantity: '1'
   });
-
-  const toppingsOptions = ['Pearls (Boba)', 'Lychee Jelly', 'Cofee Jelly', 'Honey Jelly', 'Pudding', 
-    'Crystal Boba', 'Mango Popping Boba', 'Strawberry Popping Boba', 'Ice Cream', 'Creama']
 
   // open modal for drink customization
   const openModal = (drink) => {
     setSelectedDrink(drink);
-    setModifications({size: 'Medium', sweetness: 'Normal (100%)', ice: 'Regular', toppings: [], quantity: 1});
+    setModifications({size: 'Medium', sweetness: 'Normal (100%)', ice: 'Regular', topping: 'No Toppings', quantity: 1});
   };
 
   // close modal
@@ -34,17 +54,6 @@ function Cashier({ onBack }) {
     ]);
     closeModal();
   }
-
-  // toggle toppings
-  const toggleTopping = (topping) => {
-    setModifications(prev => {
-      const newToppings = prev.toppings.includes(topping)
-        ? prev.toppings.filter(t => t !== topping)
-        : [...prev.toppings, topping];
-      return { ...prev, toppings: newToppings };
-    });
-  };
-
 
   // removes drink from cart
   const removeFromCart = (index) => {
@@ -62,6 +71,7 @@ function Cashier({ onBack }) {
           <h2>Menu</h2>
           <ul>
             <li><button onClick={onBack}>Exit</button></li>
+            {/* To add more nav spots later */}
           </ul>
         </nav>
 
@@ -69,18 +79,25 @@ function Cashier({ onBack }) {
         <div className='drink-section'>
           <h2>Choose a Drink</h2>
           <div className='drink-group'>
-            {drinks.map((drink) => (
-              <button
-                key = {drink}
-                className='drink-btn'
-                onClick={() => openModal(drink)}
-              >
-                {drink}
-              </button>
-            ))}
+            {drinks.length > 0 ? (
+              drinks.map((drink) => (
+                <button
+                  key = {drink.drink_id}
+                  className='drink-btn'
+                  onClick={() => openModal(drink)}
+                >
+                  {drink.drink_name}
+                  <br />
+                  ${Number(drink.base_price).toFixed(2)}
+                </button>
+              ))
+            ) : (
+              <p>Loading drinks...</p>
+            )}
           </div>    
         </div>
 
+        
         {/* right side of screen */}
         <div className='cart-section'>
           <h2>Cart</h2>
@@ -89,9 +106,9 @@ function Cashier({ onBack }) {
               {cart.map((item, idx) => (
               <li key={idx}>
                 <span>
-                  {item.modifications.quantity} x {item.drink} (
+                  {item.modifications.quantity} x {item.drink.drink_name} (
                   {item.modifications.size}, {item.modifications.sweetness}, {item.modifications.ice}
-                  {item.modifications.toppings.length ? ` + ${item.modifications.toppings.join(', ')}` : ''})&nbsp;
+                  {item.modifications.topping ? ` + ${item.modifications.topping}` : ''})&nbsp;
                 </span>
                 <button className='remove-btn' onClick={() => removeFromCart(idx)}>
                   X
@@ -108,10 +125,11 @@ function Cashier({ onBack }) {
             </div>  
         </div>
 
+        {/* toppings pop up */}
         {selectedDrink && (
           <div className='modal-backdrop' onClick={closeModal}>
             <div className='modal' onClick={e => e.stopPropagation()}>
-              <h3>{selectedDrink} Modifications</h3>
+              <h3>{selectedDrink.drink_name} Modifications</h3>
 
               <div className="modal-section">
                 <label>Size:</label>
@@ -154,14 +172,18 @@ function Cashier({ onBack }) {
               <div className="modal-section">
                 <label>Toppings:</label>
                 <div className="toppings-options">
-                  {toppingsOptions.map(topping => (
+                  {toppings.map(topping => (
                     <label key={topping}>
                       <input
-                        type="checkbox"
-                        checked={modifications.toppings.includes(topping)}
-                        onChange={() => toggleTopping(topping)}
-                      />
-                      {topping}
+                        type="radio"
+                        name="topping"
+                        value={topping}
+                        checked={modifications.topping === topping} 
+                        onChange={() => setModifications(prev => ({
+                          ...prev,
+                          topping
+                        }))}
+                      /> {topping}
                     </label>
                   ))}
                 </div>
